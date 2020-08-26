c7b6322148569692ff7edd9bc2b2ee8662a93d19
Bill McCutchan <bmccutchanjr@gmail.com>
2020-08-26 13:42:57 -0400

    all-products: implement 'edit' option and <form>

--- javascript/all-products.js
+++ javascript/all-products.js
@@ -1,3 +1,226 @@
+//  There are several (perhaps hundreds of) product <div> on the page, and each has an "edit" and
+//  a "delete" option.  Although a user probably wouldn't, theoretically several (even all of them)
+//  could be clicked.
+//
+//  I want to disable the "edit" and "delete" options if either is active.
+
+let deleteActive = false;
+let formActive = false;
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//  Collects the functions used to display and implement the 'confirm delete' dialog.
+
+function deleteProduct (uniqueID)
+{   //  Remove the selected <div> element from the screen and then delete the record it represents
+    //  from the database.
+
+    const product = document.getElementById ("id-" + uniqueID);
+    product.parentElement.removeChild (product);
+
+    //  and now delete the item from the database...
+
+    const transaction = db.transaction ("all-products", "readwrite");
+    const objectStore = transaction.objectStore ("all-products");
+    objectStore.delete (uniqueID);
+    transaction.onerror = event =>
+        {
+            //  Display an error...but just alert for now...
+            alert ("An error occured...");
+        };
+    transaction.onsuccess = event =>
+        {
+            //  This is all good...nothing to do...
+        };
+
+//  when this <div> is removed, the background colors of the remaining <div> will no longer alternate,
+//  because that is a consequence of the class of the siblengs and they are unaffected by removing this
+//  <div>.  So maybe iterate through the children of the parent <section> to reassign class?
+
+    //  Last things last...remove the modal window
+    hideMessage ();
+
+    deleteActive = false;
+}
+
+function confirmDelete (element)
+{
+    if (deleteActive) return;
+    if (formActive) return;
+
+    deleteActive = true;
+
+    const parent = element.parentNode;
+    const uniqueID = parent.getAttribute ("uniqueID");
+
+    const name = parent.firstChild.innerText;
+
+    const modal = configureElement ("div",
+        {   "class": "modal",
+            "id": "modal",
+            "top": window.pageYOffset + 50 + "px",
+        },
+        document.body);
+
+    const tdiv = configureElement ("div",
+        {   "class": "modal-text",
+            "innerHTML": "You have chosen to permanently delete item: " +
+                    "<b>" + name + "</b> from the database.<br><br>" +
+                    "Are you sure?"
+        },
+        modal);
+    
+    const bdiv = configureElement ("div",
+        {   "class": "modal-buttons"
+        },
+        modal);
+
+    configureElement ("a",
+        {   "class": "modal-option",
+            "href": "#",
+            "onclick": "event.preventDefault();deleteProduct (" + uniqueID + ")",
+            "innerText": "DELETE"
+        },
+        bdiv);
+
+    configureElement ("a",
+        {   "class": "modal-option",
+            "href": "#",
+            "onclick": "event.preventDefault();hideMessage ()",
+            "innerText": "CANCEL"
+        },
+        bdiv);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//  Collect the functions used to display and interact with the <form>
+
+function formCancel (event)
+{   //  The event handler for the <form> cancel option
+
+    event.preventDefault();
+
+    form.style.display = "none";
+
+    form["prod-name"].value = undefined;
+    form["hide-name"].value = undefined;
+
+    form["prod-loc"].value = undefined;
+    form["hide-loc"].value = undefined;
+
+    form["plan-qty"].value = undefined;
+    form["hide-qty"].value = undefined;
+
+    form["plan-price"].value = undefined;
+    form["hide-price"].value = undefined;
+
+    form["prod-desc"].value = undefined;
+    form["hide-desc"].value = undefined;
+
+    form["hide-uID"].value = undefined;
+
+    formActive = false;
+}
+
+function formDisplay ()
+{   form.style.display = "block";
+    form.style.top = window.pageYOffset + 50 + "px";
+}
+
+function formReset ()
+{   //  The event handler for the <form> reset option
+
+    event.preventDefault();
+    
+    form["prod-name"].value = form["hide-name"].value;
+    form["prod-loc"].value = form["hide-loc"].value;
+    form["plan-qty"].value = form["hide-qty"].value;
+    form["plan-price"].value = form["hide-price"].value;
+    form["prod-desc"].value = form["hide-desc"].value;
+}
+
+function formSave (event)
+{   //  The event handler for the <form> save option
+
+    event.preventDefault ();
+    
+    const objectStore = db.transaction (["all-products"], "readwrite")
+                          .objectStore ("all-products")
+    const r1 = objectStore.get (Number (form["hide-uID"].value));
+    r1.onsuccess = event =>
+    {   let data = event.target.result;
+
+        data.product = form["prod-name"].value;
+        data.location = form["prod-loc"].value;
+        data.description = form["prod-desc"].value;
+        data.plan.quantity = Math.floor(form["plan-qty"].value);
+        data.plan.cost = Math.floor(form["plan-price"].value * 100);
+//  01  Do I even need this?
+//  01          if (form["hide-uID"].value != "")
+//  01              data.uniqueID = form["hide-uID"].value
+
+        const r2 = objectStore.put (data);
+        r2.onsuccess = event =>
+        {   form.style.display = "none";
+            formActive = false;
+        }
+    }
+}
+
+function changeProduct (uniqueID)
+{   //  The event handler for the edit option of each product <div>
+
+    if (formActive) return;
+    if (deleteActive) return;
+
+    //  Signal that the <form> is now activated
+
+    formActive = true;
+
+    let data = {};
+
+    const transaction = db.transaction (["all-products"]);
+    const objectStore = transaction.objectStore ("all-products");
+    const request = objectStore.get (uniqueID);
+    request.onerror = event =>
+    {
+        alert ("error reading the database");
+    }
+
+    request.onsuccess = event =>
+    {
+        data = event.target.result;
+
+        form["prod-name"].value = data.product;
+        form["hide-name"].value = data.product;
+
+        form["prod-loc"].value = data.location;
+        form["hide-loc"].value = data.location;
+
+        form["plan-qty"].value = data.plan.quantity;
+        form["hide-qty"].value = data.plan.quantity;
+
+        form["plan-price"].value = formatDollars (data.plan.cost);
+        form["hide-price"].value = formatDollars (data.plan.cost);
+
+        form["prod-desc"].value = data.description;
+        form["hide-desc"].value = data.description;
+
+        form["hide-uID"].value = uniqueID;
+
+        formDisplay ();
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//  Collects the functions used to create and populate <div> elements for each product in the
+//  database.  These functions are only called when the page is first loaded (or refreshed).
+
 function makeProductDiv (section, data, even)
 {   //  Creates a <div> element that will be appended to the <section> element referenced by parameter
     //  'section'
@@ -48,9 +271,11 @@ function makeProductDiv (section, data, even)
         },
         parentDiv);
 
-    configureElement ("button",
-        {   "class": "prod-button edit",
-            "innerText": "EDIT"
+    configureElement ("a",
+        {   "class": "modal-option",
+            "href": "#",
+            "innerText": "EDIT",
+            "onclick": "event.preventDefault();changeProduct (" + data.uniqueID + ")"
         },
         parentDiv);
 
@@ -58,7 +283,7 @@ function makeProductDiv (section, data, even)
         {   "class": "prod-button delete",
             "href": "#",
             "innerText": "X",
-            "onclick": "confirmDelete (this)"
+            "onclick": "event.preventDefault();confirmDelete (this)"
         },
         parentDiv);
 }
--- javascript/common.js
+++ javascript/common.js
@@ -1,5 +1,9 @@
 //  These functions are used on more than one page in the application.
 
+//
+//  Utility functions that are used by multiple functions and processes throughout the application
+//
+
 function configureElement (elementType, object, parent = undefined)
 {   //  A generic function to configure HTML elements in the DOM.  There are half a dozen HTML
     //  elements on each line of the list and each needs to be configured and elements need to
@@ -9,7 +13,7 @@ function configureElement (elementType, object, parent = undefined)
     //  Thus this function.  Write the code one time...
     //
     //  'parent' is an optional parameter.  If passed it is some DOM element that the newly created
-    //  element will appended to.
+    //  element will be appended to.
 
     //  First thing is to create the new HTML element...
     let element = document.createElement (elementType);
@@ -26,6 +30,11 @@ function configureElement (elementType, object, parent = undefined)
             {   element.innerHTML = attribute[1];
                 break;
             }
+            case "top":
+            {   
+                element.style.top = attribute[1];
+                break;
+            }
             default:
             {   element.setAttribute (attribute[0], attribute[1]);
                 break;
@@ -40,112 +49,36 @@ function configureElement (elementType, object, parent = undefined)
     return element;
 }
 
-function displayMessage (message)
-{   //  Configure and display the message modal
-    //
-    //  In an attempt to make this function as generic as possible, the modal (including buttons) is
-    //  built from scratch.
-
-    const section = document.getElementById ("message");
-    while (section.hasChildNodes ())
-    {   section.removeChile (section.firstChild)
-    }
-
-    const array = message.entries ();
-    array.forEach (a =>
-    {   switch (a[0])
-        {
-            case ("button"):
-            {   const buttons = section.getElementsByClassName ("button-bar");
-
-                const div = buttons.entries ();
-                div.forEach (b =>
-                {
-                    configureElement ("a",
-                        {
-                           "class": b.class,
-                           "onclick": b.onclick 
-                        },
-                        d);
-                })
-
-                break;
-            }
-            case "class":
-            {
-                section.setAttribute ("class", section.setAttribute ("class") + a[1]);
-                break;
-            }
-            case "message":
-            {
-                section.innerText = a[1];
-                break;
-            }
-            default:
-            {   //  Not sure there is a default function
-                break;
-            }
-        }
-
-    })
+function formatDollars (number)
+{   return (number / 100).toFixed (2)
 }
 
 function hideMessage ()
-{   //  A generic function to "hide" the modal window that messages are displayed in.  'hide' is a
-    //  euphamism for remove the element from the DOM, which both removes it from the screen and
-    //  removes any content that might be in it.
+{   //  A utility that "hides" the modal window used to display messages.  This function is called
+    //  by several processes and functions throughout the application.
+    // 
+    //  Note that 'hide' is a euphamism for 'remove'.  The modal <div> will be removed from the DOM,
+    //  which not only gets it off the screen but also removes any content (including child nodes)
+    //  that might be in it.
 
     document.body.removeChild (document.getElementById ("modal"));
+
+    deleteActive = false;
 }
 
-function deleteProduct (uniqueID)
-{   //  Remove the selected <div> element from the screen and then delete the record it represents
-    //  from the database.
-
-    const product = document.getElementById ("id-" + uniqueID);
-    product.parentElement.removeChild (product);
-
-//  and now delete the item from the database...
-
-    const transaction = db.transaction ("all-products", "readwrite");
-    const objectStore = transaction.objectStore ("all-products");
-    objectStore.delete (uniqueID);
-    transaction.onerror = event =>
-        {
-            //  Display an error...but just alert for now...
-            alert ("An error occured...");
-        };
-    transaction.onsuccess = event =>
-        {
-            //  This is all good...nothing to do...
-        };
-
-//  when this <div> is removed, the background colors of the remaining <div> will no longer alternate,
-//  because that is a consequence of the class of the siblengs and they are unaffected by removing this
-//  <div>.  So maybe iterate through the children of the parent <section> to reassign class?
-
-    //  Last things last...remove the modal window
-    hideMessage ();
-}
-
-function confirmDelete (element)
-{
-    const parent = element.parentNode;
-    const uniqueID = parent.getAttribute ("uniqueID");
-
-    const name = parent.firstChild.innerText;
+function noData ()
+{   //  There is nothing in the database...prompt the user to add some...
 
     const modal = configureElement ("div",
         {   "class": "modal",
-            "id": "modal"
+            "id": "modal",
+            "top": "150px",
         },
         document.body);
 
     const tdiv = configureElement ("div",
         {   "class": "modal-text",
-            "innerHTML": "You have chosen to permanently delete item: " +
-                    "<b>" + name + "</b> from the database.<br><br>" +
-                    "Are you sure?"
+            "innerHTML": "There is nothing in the database.  Why not add some?"
         },
         modal);
     
@@ -157,40 +90,28 @@ function confirmDelete (element)
     configureElement ("a",
         {   "class": "modal-option",
             "href": "#",
-            "onclick": "deleteProduct (" + uniqueID + ")",
-            "innerText": "DELETE"
-        },
-        bdiv);
-
-    configureElement ("a",
-        {   "class": "modal-option",
-            "href": "#",
-            "onclick": "hideMessage ()",
-            "innerText": "CANCEL"
+            "onclick": "event.preventDefault();addProduct ()",
+            "innerText": "ADD"
         },
         bdiv);
 }
 
-function formatDollars (number)
-{   return (number / 100).toFixed (2)
-}
-
-function errorMessage (message)
-{   //  Use configureElement() to place the message on the screen.
-    configureElement ("div",
-        {   "class": "message error",
-            "innerText": message
-        },
-        document.getElementById ("main"));
-}
-
-function workingMessage ()
-{   //  Why display an empty screen for the time it takes to open and read the database?  Use
-    //  configureElement() to let the user know the application is working.
-
-    configureElement ("div",
-        {   "class": "message",
-            "innerText": "No data yet.  Waiting on the database."
-        },
-        document.getElementById ("main"));
-}
+//  004 function errorMessage (message)
+//  004 {   //  Use configureElement() to place the message on the screen.
+//  004     configureElement ("div",
+//  004         {   "class": "message error",
+//  004             "innerText": message
+//  004         },
+//  004         document.getElementById ("main"));
+//  004 }
+//  004
+//  004 function workingMessage ()
+//  004 {   //  Why display an empty screen for the time it takes to open and read the database?  Use
+//  004     //  configureElement() to let the user know the application is working.
+//  004
+//  004     configureElement ("div",
+//  004         {   "class": "message",
+//  004             "innerText": "No data yet.  Waiting on the database."
+//  004         },
+//  004         document.getElementById ("main"));
+//  004 }
