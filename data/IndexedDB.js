93244b8d41885192734faceb4ee5ea51a49031e6
Bill McCutchan <bmccutchanjr@gmail.com>
2020-08-26 13:58:51 -0400

    wrap indexedDB.open() request in a function

--- data/IndexedDB.js
+++ data/IndexedDB.js
@@ -2,49 +2,68 @@ console.log ("IndexedDB.js");
 
 let db = undefined;     //  a reference to the database
 
-const request = indexedDB.open ("shopping.list", 1);
-request.onerror = event =>
-{   console.log ("failure to launch");
-}
+function getDbReference ()
+{   //  Open the database and set a global variable to reference the database object throughout
+    //  the application.
 
-request.onsuccess = event =>
-{   db = event.target.result;
+    //  This code does not have to be implemented as a function.  Nothing about it screams function;
+    //  -   It will work just fine without being enclosed in a function.  In fact, when I first
+    //      wrote it it was not.
+    //  -   This code is used exactly once.  It will be referenced nowhere else in this application.
+    //
+    //  But without wrapping the code in a function, every variable declared here will be a
+    //  global variable and exist throughout the application.  The only variable declared here that
+    //  needs a reference elsewhere is 'db'.  Everything else is only required to get 'db'.
+    //
+    //  The function allows JavaScript to do some housecleaning and remove those extraneous
+    //  references.
+    
+    const request = indexedDB.open ("shopping.list", 1);
+    request.onerror = event =>
+    {   console.log ("failure to launch");
+    }
 
-    const transaction = db.transaction ("all-products");
-    const objectStore = transaction.objectStore ("all-products")
-    const index = objectStore.index ("by-location-product")
+    request.onsuccess = event =>
+    {   db = event.target.result;
 
-    let count = 0;
-    let locationcount = 0;
-    let lastlocation = undefined;
-    let section = undefined;
+        const transaction = db.transaction ("all-products");
+        const objectStore = transaction.objectStore ("all-products")
+        const index = objectStore.index ("by-location-product")
 
-    const main = document.getElementById ("main");
+        let count = 0;
+        let locationcount = 0;
+        let lastlocation = undefined;
+        let section = undefined;
 
-    index.openCursor().onsuccess = (event =>
-    {   let cursor = event.target.result;
+        const main = document.getElementById ("main");
 
-        if (cursor)
-        {
-            ++count;
+        index.openCursor().onsuccess = (event =>
+        {   let cursor = event.target.result;
 
-            if (cursor.value.location != lastlocation)
-            {   lastlocation = cursor.value.location
-                locationcount = 0;
+            if (cursor)
+            {
+                ++count;
 
-                section = document.createElement ("section");
-                main.append (section);
+                if (cursor.value.location != lastlocation)
+                {   lastlocation = cursor.value.location
+                    locationcount = 0;
 
-                configureElement ("div",
-                    {   "class": "heading",
-                        "innerText": cursor.value.location.toUpperCase() + " ▼",
-                        "onclick": "console.log ('heading clicked');"
-                    },
-                    section);
+                    section = document.createElement ("section");
+                    main.append (section);
+
+                    configureElement ("div",
+                        {   "class": "heading",
+                            "innerText": cursor.value.location.toUpperCase() + " ▼",
+                            "onclick": "console.log ('heading clicked');"
+                        },
+                        section);
+                }
+
+                buildPage (section, cursor, ((++locationcount % 2) == 0));
+                cursor.continue ();
             }
-
-            buildPage (section, cursor, ((++locationcount % 2) == 0));
-            cursor.continue ();
-        }
-    })
+        })
+    }
 }
+
+getDbReference ();
